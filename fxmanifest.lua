14776bf062579d56fa3afc2125f0b25d98cc8473
Zuid-Limburg Roleplay <97765699+ZuidLimburg@users.noreply.github.com>
2022-02-07 04:38:08 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description "MsQuerade's Compass and Streetname HUD - Command made by Daan#9222 :))"
+version "1.0"
+
+client_scripts {
+	'essentials.lua',
+	'compass.lua',
+	'streetname.lua'
+}
